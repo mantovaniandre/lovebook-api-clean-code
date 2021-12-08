33f7a83df6a3cef9dc1bdd907ea3cd39906d06f0
mantovaniandre <andreluizdiasmantovani@gmail.com>
2021-12-07 21:55:07 -0300

    Atualização

--- lovebook/src/main/java/br/com/lovebook/Teste.java
+++ lovebook/src/main/java/br/com/lovebook/Teste.java
@@ -7,6 +7,7 @@ public class Teste {
 	public static void main(String[] args) {
 		
 		System.out.println(new BCryptPasswordEncoder().encode("123456"));
+		System.out.println(new BCryptPasswordEncoder().encode("123"));
 
 	}
 
--- lovebook/src/main/java/br/com/lovebook/dto/UsuarioDto.java
+++ lovebook/src/main/java/br/com/lovebook/dto/UsuarioDto.java
@@ -2,6 +2,7 @@ package br.com.lovebook.dto;
 
 import java.util.Optional;
 
+import br.com.lovebook.model.Perfil;
 import br.com.lovebook.model.Usuario;
 
 public class UsuarioDto {
@@ -13,6 +14,8 @@ public class UsuarioDto {
 	private String cepUsuario;
 	private String cidadeUsuario;
 	private String estadoUsuario;
+	private String senhaUsuario;
+	private Perfil tipoUsuario;
 
 	public UsuarioDto(Optional<Usuario> user) {
 		this.nome = user.get().getNomeUsuario();
@@ -22,7 +25,12 @@ public class UsuarioDto {
 		this.cepUsuario = user.get().getCepUsuario();
 		this.cidadeUsuario = user.get().getCidadeUsuario();
 		this.estadoUsuario = user.get().getEstadoUsuario();
+		this.senhaUsuario = user.get().getPassword();
+		this.tipoUsuario = user.get().getPerfil();
+	
 	}
+
+	
 
 	public UsuarioDto(Usuario user) {
 		this.nome = user.getNomeUsuario();
@@ -32,6 +40,8 @@ public class UsuarioDto {
 		this.cepUsuario = user.getCepUsuario();
 		this.cidadeUsuario = user.getCidadeUsuario();
 		this.estadoUsuario = user.getEstadoUsuario();
+		this.senhaUsuario = user.getPassword();
+		this.tipoUsuario = user.getPerfil();
 	}
 
 	public String getNome() {
@@ -88,6 +98,22 @@ public class UsuarioDto {
 
 	public void setEstadoUsuario(String estadoUsuario) {
 		this.estadoUsuario = estadoUsuario;
+	}
+	
+	public String getSenhaUsuario() {
+		return senhaUsuario;
+	}
+
+	public void setSenhaUsuario(String senhaUsuario) {
+		this.senhaUsuario = senhaUsuario;
+	}
+	
+	public Perfil getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(Perfil tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
 	}
 
 }
